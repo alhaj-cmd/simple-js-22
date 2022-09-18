3e9f48a32f872ec561ab59c35c1f69907f5ce3fb
github_username <you@domain.com>
2022-09-18 09:29:43 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+const country ='Bangladesh';
+const num = 23;
+const object ={name:'ali',id:12, class:'two'};
+const numbers =[10,20,43,52,62,42,65,50];
+const idBangladesh = true;
+
+console.log(typeof object);
+console.log(Array.isArray(numbers));
+console.log(typeof idBangladesh);
